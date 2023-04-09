8ae387d3f263d514a9d94edc992fd5d6c5efb3f3
mahadiihasan <sagor.gsc15@gmail.com>
2023-04-09 18:05:38 +0600

    understanding nested route, outlet

--- src/main.jsx
+++ src/main.jsx
@@ -7,28 +7,54 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import ErrorPage from './ErrorPage';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App/>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>,
+//     errorElement: <ErrorPage></ErrorPage>
+//   },
+//   {
+//     path: '/contact',
+//     element: <Contact></Contact> ,
+//     errorElement: <ErrorPage></ErrorPage>
+//   }
+// ])
 
 const router = createBrowserRouter([
+
   {
     path: '/',
-    element: <App/>,
-    errorElement: <ErrorPage></ErrorPage>
-  },
-  {
-    path: '/about',
-    element: <div>this is about page</div>,
-    errorElement: <ErrorPage></ErrorPage>
-  },
-  {
-    path: '/contact',
-    element: <div>contact us right now</div>,
-    errorElement: <ErrorPage></ErrorPage>
+    element: <Home></Home>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
+
 ])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <Header></Header> */}
     <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
